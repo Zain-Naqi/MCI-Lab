abec17d3a636f9506e893473a353923d923aa030
Zain-Naqi <zainjawad50@yahoo.com>
2025-07-28 02:32:06 +0500

    Commit Lab08

---
+++ Lab08/Task01/build/Debug/CMakeFiles/3.28.1/CMakeSystem.cmake
@@ -0,0 +1,15 @@
+set(CMAKE_HOST_SYSTEM "Linux-6.11.0-29-generic")
+set(CMAKE_HOST_SYSTEM_NAME "Linux")
+set(CMAKE_HOST_SYSTEM_VERSION "6.11.0-29-generic")
+set(CMAKE_HOST_SYSTEM_PROCESSOR "x86_64")
+
+include("/media/zain-naqi/5C182024181FFBA0/Tehqiq-1/MCI-Lab/Lab08/Task01/cmake/gcc-arm-none-eabi.cmake")
+
+set(CMAKE_SYSTEM "Generic")
+set(CMAKE_SYSTEM_NAME "Generic")
+set(CMAKE_SYSTEM_VERSION "")
+set(CMAKE_SYSTEM_PROCESSOR "arm")
+
+set(CMAKE_CROSSCOMPILING "TRUE")
+
+set(CMAKE_SYSTEM_LOADED 1)
